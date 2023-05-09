37462a16d0c0fb8ef2e6903fcb963aa4900b0641
Sincerity7788 <dei@live.cn>
2023-05-09 15:34:06 +0800

    修改入口文件的测试代码

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import Vue from "./core";
-import {parsePath} from "./core/util";
-const fn = parsePath("aa.bb.cc")
-const obj = {
-  aa: {
-    bb: {
-      cc: "这个是最里层的数据"
-    }
-  }
-}
-console.log(fn, fn(obj))
-console.log(Vue);
-console.log("入口文件")
+console.log("入口文件");
